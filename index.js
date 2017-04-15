75b6ed0aed10791f88bac5e7d8d17f6b3d70914d
mohayonao <mohayonao@gmail.com>
2017-04-15 09:51:45 +0900

    fix for MS Edge
    related https://github.com/mohayonao/stereo-panner-node/issues/11

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ var AudioContext = global.AudioContext || global.webkitAudioContext;
 var OfflineAudioContext = global.OfflineAudioContext || global.webkitOfflineAudioContext;
 var BaseAudioContext = global.BaseAudioContext || (OfflineAudioContext && Object.getPrototypeOf(OfflineAudioContext));
 
-module.exports = typeof BaseAudioContext === "function" ? BaseAudioContext : AudioContext;
+module.exports = (typeof BaseAudioContext === "function" && BaseAudioContext.prototype) ? BaseAudioContext : AudioContext;
